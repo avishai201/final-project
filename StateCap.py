b028d004e0dd30c6a4801df8a359d6376c284439
avishai <avishai.shaked@gmail.com>
2021-07-31 09:40:29 +0300

    Fixed the code

--- StateCap.py
+++ StateCap.py
@@ -1,95 +1,62 @@
-"""We have an existing dictionary that maps US states to their capitals.
-1. Print the state capital of Idaho
-2. Print all states.
-3. Print all capitals.
-4. Create a single string 'Alabama -> Montgomery, Alaska -> Juneau, ...'
-5. Ensure the string you created in 4. is alphabetically sorted by state
-7. Now we want to add the reverse look up, given the name of a capital what state
-is it in?
-Implement the function def get_state(capital): below so it returns the state.
-GOTCHAS: What happens if two states have the same capital name, how do you
-handle that?
+import sys
+import pytest
+import ast
+
+
+def dic_file():
+    path_file = "/Users/avishai/IdeaProjects/final-project/capital_dic"  # path of dic file
+    with open(path_file, "r") as data:  # read permission only
+        dict = ast.literal_eval(data.read())
+    return dict
+
+
+# 1. Print the state capital of Idaho
+def capital_of_Idaho():
+    Idaho_capital = dic_file().get("Idaho", "")
+    print(f"The capital city of Idaho is {Idaho_capital}\n")
+
+
+# 2. Print all states.
+def all_states():
+    for states in dic_file().keys():
+        print(states)
+
+
+# 3. Print all capitals.
+def all_capitals():
+    for capitals in dic_file().values():
+        print(capitals)
+
+
+#4. Create a single string 'Alabama -> Montgomery, Alaska -> Juneau, ...'
+def states_capitals_string():
+    x = ','.join(' --> '.join((key, val)) for (key, val) in dic_file().items())
+    print(x)
+
+
+# 5. Ensure the string you created in 4. is alphabetically sorted by state
+def states_capitals_string_alphabetically():
+    for i in sorted(dic_file().keys()):
+        print(i, end=" ")
+
+
+# Get State per a capital city
+def get_state(capital):
+    for key, value in dic_file().items():  #  key = state
+        if capital == value:
+            print(f"{capital} is the capital of {key}")
+
+
+# Get capital city per state
+def get_capital(state):
+    for key, value in dic_file().items():
+        if state == key:
+            print(f"{value} is the capital of {state}")
+
 
 """
-import sys
 
-import pytest
-
-STATES_CAPITALS = {
-    'Alabama' : 'Montgomery',
-    'Alaska' : 'Juneau',
-    'Arizona' : 'Phoenix',
-    'Arkansas': 'Little Rock',
-    'California' : 'Sacramento',
-    'Colorado' : 'Denver',
-    'Connecticut' : 'Hartford',
-    'Delaware' : 'Dover',
-    'Florida' : 'Tallahassee',
-    'Georgia' : 'Atlanta',
-    'Hawaii' : 'Honolulu',
-    'Idaho' : 'Boise',
-    'Illinois' : 'Springfield',
-    'Indiana' : 'Indianapolis',
-    'Iowa' : 'Des Moines',
-    'Kansas' : 'Topeka',
-    'Kentucky' : 'Frankfort',
-    'Louisiana' : 'Baton Rouge',
-    'Maine' : 'Augusta',
-    'Maryland' : 'Annapolis',
-    'Massachusetts' : 'Boston',
-    'Michigan' : 'Lansing',
-    'Minnesota' : 'Saint Paul',
-    'Mississippi' : 'Jackson',
-    'Missouri' : 'Jefferson City',
-    'Montana' : 'Helena',
-    'Nebraska' : 'Lincoln',
-    'Nevada' : 'Carson City',
-    'New Hampshire' : 'Concord',
-    'New Jersey' : 'Trenton',
-    'New Mexico' : 'Santa Fe',
-    'New York' : 'Albany',
-    'North Carolina' : 'Raleigh',
-    'North Dakota' : 'Bismarck',
-    'Ohio' : 'Columbus',
-    'Oklahoma' : 'Oklahoma City',
-    'Oregon' : 'Salem',
-    'Pennsylvania' : 'Harrisburg',
-    'Rhode Island' : 'Providence',
-    'South Carolina' : 'Columbia',
-    'South Dakota' : 'Pierre',
-    'Tennessee' : 'Nashville',
-    'Texas' : 'Austin',
-    'Utah' : 'Salt Lake City',
-    'Vermont' : 'Montpelier',
-    'Virginia' : 'Richmond',
-    'Washington' : 'Olympia',
-    'West Virginia' : 'Charleston',
-    'Wisconsin' : 'Madison',
-    'Wyoming' : 'Cheyenne',
-}
-
-
-def capital_of_Idaho():
-    # Your code here
-    pass
-
-def all_states():
-    # Your code here
-    pass
-
-def all_capitals():
-    # Your code here
-    pass
-
-def states_capitals_string():
-    # Your code here
-    pass
-
-
-
-def get_state(capital):
-    pass
-
+### Test my code ###
 
 
 def test_state_to_capital():
@@ -105,14 +72,25 @@ def test_capital_to_state():
     assert 'Wyoming' == get_state('Cheyenne')
 
 
+
 def test_capital_to_state_unknown():
     with pytest.raises(KeyError):
         get_state('')
+
+
+def Results(Idaho_capital, states, capitas):
+    print(Idaho_capital)
+    print(states)
+    print(capitas)
 
 
 def main():
     return pytest.main(__file__)
 
 
+
 if __name__ == '__main__':
     sys.exit(main())
+
+"""
+
